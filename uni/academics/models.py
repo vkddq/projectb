69d3394f701b6bc879f31de9a3cdce718995edea
vkddq <vovanoha42@gmail.com>
2026-05-27 14:13:36 +0300

    Fix

--- uni/academics/models.py
+++ uni/academics/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from branches.models import Branch
+from django.conf import settings
 
 class Subject(models.Model):
     name = models.CharField(max_length=100)
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='subjects')
     is_active = models.BooleanField(default=True)
+    teacher = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name='subjects_taught')
 
     class Meta:
         unique_together = ('name', 'branch') 
