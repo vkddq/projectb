7ceba989f178dc5e3a2ecab338bd5115192d8b1f
vkddq <vovanoha42@gmail.com>
2026-05-27 13:33:13 +0300

    Fix

--- uni/uni/settings.py
+++ uni/uni/settings.py
@@ -86,12 +86,11 @@ WSGI_APPLICATION = 'uni.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
