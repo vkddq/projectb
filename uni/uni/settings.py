69fdbbe3f3b08810d3b8018f186e912c9ef7a036
vkddq <vovanoha42@gmail.com>
2026-05-27 12:08:01 +0300

    PostgreSQL

--- uni/uni/settings.py
+++ uni/uni/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -168,3 +170,11 @@ SPECTACULAR_SETTINGS = {
 }
 
 LOGIN_URL = '/login/'
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
+
+}
